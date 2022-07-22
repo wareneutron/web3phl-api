e3d0ba08ecf9b27e3d0b7deb9f78ea142b79921e
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-22 20:51:07 +0800

    add: models and data

---
+++ models/directoryModel.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+// builder pattern
+class Directory {
+  #filepath; // private
+  #name;
+  #tags;
+
+  /*
+   * @param path - path to JSON file
+   */
+  constructor(path) {
+    this.#filepath= path;
+  }
+  
+  /*
+   * @param searchName - name string to look for from data source
+   */
+  name(searchName) {
+    if (!searchName || searchName === undefined) return this;
+
+    this.#name = searchName;
+    return this;
+  }
+
+  /*
+   * @param {string[]} tagsList - one or more tags to use for filtering
+   */
+  tags(tagsList) {
+    if (!tagsList || tagsList === undefined) return this;
+
+    // convert string to array of strings
+    let tags = tagsList.split(",");
+    this.#tags = tags;
+    return this;
+  }
+
+  // TODO: sort builder method
+
+  get() {
+    // process query
+    let data = JSON.parse(fs.readFileSync(this.#filepath, 'utf8'));
+
+    // filter by name
+    if (this.#name && this.#name !== undefined) {
+      data = data.filter((e) => {
+        return e.name.toLowerCase().includes(this.#name.toLowerCase());
+      });
+    }
+    
+    // filter by tags
+    if (this.#tags && !this.#tags.length !== 0) {
+      this.#tags.forEach(t => {
+        data = data.filter(e => e.tags.includes(t));
+      });
+    }
+
+
+    // sort result alphabetically
+    data.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
+    return data;
+  }
+}
+
+// export instance of directory
+module.exports = new Directory('./data/directory.json');
